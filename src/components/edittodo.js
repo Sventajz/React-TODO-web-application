a494358baab8c249e727b778ee0c65e10ecc73fc
sven tajz <sventajz3@gmail.com>
2024-01-30 17:01:05 +0100

    first commit

---
+++ src/components/edittodo.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const edittodo = () => {
+  return <div>edittodo</div>;
+};
---
+++ src/components/todoform.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const todoform = () => {
+  return <div>todoform</div>;
+};
